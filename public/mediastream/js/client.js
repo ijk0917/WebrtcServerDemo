31f080b5caf6929dd51fa404c4be371120ac6f88
heqi <645605514@qq.com>
2019-10-24 15:18:49 +0800

    1.视频截图

--- public/mediastream/js/client.js
+++ public/mediastream/js/client.js
@@ -1,24 +1,19 @@
 'use strict'
 
+//devices
 var audioSource = document.querySelector('select#audioSource');
 var audioOutput = document.querySelector('select#audioOutput');
 var videoSource = document.querySelector('select#videoSource');
+
+//filter
+var filterSelect = document.querySelector('select#filter');
+
+//picture
+var snapshot = document.querySelector('button#snapshot');
+var picture = document.querySelector('canvas#picture');
+
 var videoPlayer = document.querySelector('video#player');
-
-
-if (!navigator.mediaDevices ||
-    !navigator.mediaDevices.getUserMedia) {
-    console.log('getUserMedia is not supported!')
-} else {
-    var constraints = {
-        video: true,
-        audie: true
-    }
-    navigator.mediaDevices.getUserMedia(constraints)
-        .then(gotMediaStream)
-        .then(gotDevices)
-        .catch(handleError)
-}
+// var audioPlayer = document.querySelector('audio#audioplayer');
 
 function gotMediaStream(stream) {
     videoPlayer.srcObject = stream;
@@ -32,23 +27,55 @@ function gotDevices(deviceInfos) {
         option.text = deviceInfo.label;
         option.value = deviceInfo.deviceId;
 
-        if(deviceInfo.kind === 'audioinput'){
+        if (deviceInfo.kind === 'audioinput') {
             audioSource.appendChild(option);
-        }else if(deviceInfo.kind === 'audiooutput'){
+        } else if (deviceInfo.kind === 'audiooutput') {
             audioOutput.appendChild(option);
-        }else if(deviceInfo.kind === 'videoinput'){
+        } else if (deviceInfo.kind === 'videoinput') {
             videoSource.appendChild(option);
         }
-
-        // console.log(
-        //     "kind = " + deviceInfo.kind + ":" +
-        //     "label = " + deviceInfo.label + ":" +
-        //     "deviceId = " + deviceInfo.deviceId + ":" +
-        //     "groupId = " + deviceInfo.groupId
-        // )
     })
 }
 
 function handleError(err) {
-    console.log(err.name + ": " + err.message);
+    console.log('getUserMedia error: ' + err);
+}
+
+function start() {
+    if (!navigator.mediaDevices ||
+        !navigator.mediaDevices.getUserMedia) {
+        console.log('getUserMedia is not supported!')
+    } else {
+
+        var deviceId = videoSource.value;
+        var constraints = {
+            video: {
+                width: 640
+                , height: 480
+                , frameRate: 30
+                , facingMode: 'enviroment'
+                , deviceId: deviceId ? deviceId : undefined
+            },
+            audie: {
+                noiseSuppression: true
+                , echoCancellation: true
+            }
+        }
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(gotMediaStream)
+            .then(gotDevices)
+            .catch(handleError)
+    }
+}
+
+start();
+
+videoSource.onchange = start();
+
+filterSelect.onchange = function () {
+    videoPlayer.className = filterSelect.value;
+}
+
+snapshot.onclick = function () {
+    picture.getContext('2d').drawImage(videoPlayer, 0, 0, 320, 240);
 }
